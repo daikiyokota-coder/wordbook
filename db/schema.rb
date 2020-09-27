9ecc1a2beb53404b242a9e4f5efddd2a4486f57e
yokotadaiki <yd43star@gmail.com>
2020-09-27 19:38:36 +0900

    追加: Userモデルにpresence: trueのバリデーションを追加した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_100639) do
+ActiveRecord::Schema.define(version: 2020_09_27_103113) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
