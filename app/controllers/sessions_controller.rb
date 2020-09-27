cce9e6b6068feb7670c1f62083f209f0ccd52828
yokotadaiki <yd43star@gmail.com>
2020-09-28 01:38:07 +0900

    追加: sessions_helper.rbに3つのメソッドを定義した。
    log_inメソッドとcurrent_userメソッドとlogged_in?メソッドを定義した。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: session_params[:name])
     if user&.authenticate(session_params[:password])
-      session[:user_id] = user.id
+      log_in(user)
       redirect_to root_path, notice: 'ログインしました'
     else
       flash.now[:danger] = 'ユーザ名またはパスワードが違います'
