37b8426ac1ea2139a3c8f33e06d284e8f613a66c
yokotadaiki <yd43star@gmail.com>
2020-09-29 20:29:55 +0900

    追加: models/question_spec.rbにバリデーションのテストを追加した。

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   factory :question do
+    question { 'Ruby' }
+    description { 'プログラミング言語' }
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @question = build(:question)
+  end
+
+  describe 'バリデーション' do
+    it 'questionとdescriptionの2つの値が設定された単語は有効' do
+      expect(@question.valid?).to eq(true)
+    end
+
+    it 'questionが設定されていない単語はNG' do
+      @question.question = ''
+      expect(@question.valid?).to eq(false)
+    end
+
+    it 'descriptionが設定されていない単語はNG' do
+      @question.description = ''
+      expect(@question.valid?).to eq(false)
+    end
+  end
 end
