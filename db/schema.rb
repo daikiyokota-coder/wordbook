7cda90b7f557eeea4dccbb175efaec70f9427299
yokotadaiki <yd43star@gmail.com>
2020-09-30 15:33:07 +0900

    追加: question_similarモデルを作成した。
    question_id:integer similar_word:stringでカラムを追加した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_051034) do
+ActiveRecord::Schema.define(version: 2020_09_30_062957) do
+
+  create_table "question_similars", force: :cascade do |t|
+    t.integer "question_id"
+    t.string "similar_word"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
---
+++ spec/factories/question_similars.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :question_similar do
+    question_id { 1 }
+    similar_word { "MyString" }
+  end
+end
