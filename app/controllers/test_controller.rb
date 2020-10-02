e5927fc56dcae14b8c1318d18481d4d105bef5d9
yokotadaiki <yd43star@gmail.com>
2020-10-02 14:57:06 +0900

    追加: 問題を1問解答すると、次の問題からは先ほど解答した問題は出ないようにした。
    既出の単語のidをsessionに保存して、 Question.allから既出の単語を削除している
    questions = Question.all
    destroy_questions = session[:question_ids].map { |n| Question.find(n.to_i) }
    questions -= destroy_questions

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -6,6 +6,7 @@ class TestController < ApplicationController
     session[:correct] = 0
     session[:incorrect] = 0
     session[:number] = 1
+    session[:question_ids] = []
     questions = Question.all
     @question = questions.order("RANDOM()").first
     questions -= [@question]
@@ -20,9 +21,17 @@ class TestController < ApplicationController
       session[:incorrect] += 1
     end
     session[:number] += 1
+    # 既出の単語のidを保存している
+    session[:question_ids] << params[:correct_question_id]
+    # 単語の集合から既出の単語を削除している
     questions = Question.all
-    @question = questions.order("RANDOM()").first
+    destroy_questions = session[:question_ids].map { |n| Question.find(n.to_i) }
+    questions -= destroy_questions
+    # @question = questions.order("RANDOM()").first
+    # 残った単語の集合から出題する問題を抜き出す
+    @question = questions.sample
     questions -= [@question]
+    # 3択の残り2つを抜き出す
     incorrect_questions = questions.sample(2)
     @question_descriptions = incorrect_questions + [@question]
   end
