9c5fe56888830e3def8ac604ea8c00791d8cc8e5
yokotadaiki <yd43star@gmail.com>
2020-10-02 17:05:31 +0900

    追加: Usersテーブルにhighest_rateカラムを追加した。データ型はinteger

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_062957) do
+ActiveRecord::Schema.define(version: 2020_10_02_080214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_09_30_062957) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "highest_rate"
   end
 
 end
