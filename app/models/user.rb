e1a9112fff9275ec69fcfa5f8d448e4a739b052a
yokotadaiki <yd43star@gmail.com>
2020-10-05 16:06:26 +0900

    追加: ranking.html.erbにcssを追加してデザインを整えた。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
   validates :password_digest, presence: true
 end
