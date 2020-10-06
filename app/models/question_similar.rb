ce2f13a9dceced2669b02ccd7755a05d5ec2cba1
yokotadaiki <yd43star@gmail.com>
2020-10-06 18:54:40 +0900

    改善: 類義語名にnull falseを設定した。さらに類義語を空欄で送った場合は自動で類義語名が入るようにした。

--- app/models/question_similar.rb
+++ app/models/question_similar.rb
@@ -1,4 +1,11 @@
 class QuestionSimilar < ApplicationRecord
+  before_save :blank_to_nil
   belongs_to :question
-  validates :similar_word, presence: true
+  # validates :similar_word, presence: true
+
+  def blank_to_nil
+    if similar_word == ""
+      self.similar_word = "#{question.question}の類義語"
+    end
+  end
 end
