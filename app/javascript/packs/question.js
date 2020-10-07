ae5cf33a456b4fc742fde733b53508cc83b80dd4
yokotadaiki <yd43star@gmail.com>
2020-10-07 11:26:02 +0900

    修正: question.jsの変数名とquestions/new.html.erbのインデントを整えた。

--- app/javascript/packs/question.js
+++ app/javascript/packs/question.js
@@ -2,10 +2,10 @@ $(function(){
     $("#question_similar_form").hide();
     $("#question_similar_qlus").click(function(){
       $("#question_similar_form").slideToggle("slow");
-      var test = $("#question_similar_qlus").html();
-      if (test == '類義語を追加+') {
+      var similar_form_text = $("#question_similar_qlus").html();
+      if (similar_form_text == '類義語を追加+') {
           $("#question_similar_qlus").html('フォームを閉じる')
-      } else if (test == 'フォームを閉じる') {
+      } else if (similar_form_text == 'フォームを閉じる') {
           $("#question_similar_qlus").html('類義語を追加+')
       }
     });
